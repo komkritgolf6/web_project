ecad50f14f0010accec61fcab1fef335a16b65ba
golf <komkritgolf6@gmail.com>
2024-05-15 15:18:51 +0700

    test

--- app/routes.py
+++ app/routes.py
@@ -6,9 +6,16 @@ bp = Blueprint('routes', __name__)
 @bp.route('/')
 def home():
     print("ok")
-    return render_template('base.html')
+    return render_template('login.html')
 
 @bp.route('/protected', methods=['GET'])
 @jwt_required()
 def protected():
     return jsonify(message="This is a protected route")
+
+@bp.route('/register')
+def regis():
+    print("ok")
+    return render_template('Register.html')
+
+
