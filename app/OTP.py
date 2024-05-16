8d714dd90bb85fd31340308a8622dc8d4bcd7167
golf <komkritgolf6@gmail.com>
2024-05-16 09:54:17 +0700

    otpemail

--- app/OTP.py
+++ app/OTP.py
@@ -1,6 +1,8 @@
 import random
 import string
-import time
+# import time
+from email.mime.text import MIMEText
+import smtplib
 
 def generate_otp(length=6):
     # สร้างรหัส OTP จากตัวอักษรและตัวเลข
@@ -18,3 +20,14 @@ print("Your OTP is:", OTP_Code)
 # หลังจากผ่านไป 1 นาที ให้สร้าง OTP ใหม่
 OTP_Code = generate_otp()
 print("Your new OTP is:", OTP_Code)
+
+def send_otp_to_email(email, otp):
+    msg = MIMEText(f"Your OTP is: {otp}")
+    msg['Subject'] = 'Your OTP Code'
+    msg['From'] = 'komkritgolf6@gmail.com'
+    msg['To'] = email
+    print("otp email:", otp)
+    with smtplib.SMTP('smtp.example.com') as server:
+        server.login('your-email@example.com', 'your-password')
+        server.sendmail('your-email@example.com', [email], msg.as_string())
+        
--- app/models.py
+++ app/models.py
@@ -6,8 +6,10 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), unique=True,nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=True)
     otp = db.Column(db.String(200), nullable=True)
-    token = db.Column(db.String(200), nullable=True)
+    
+    
 
 def create_table():
     db.create_all()
